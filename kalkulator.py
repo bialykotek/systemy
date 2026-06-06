1a71bc2f0c66171b7ace62004fc7df5bdf51cf82
bialykotek <kuba78552@gmail.com>
2026-06-06 10:19:35 -0400

    Dodano kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,11 @@
+a = float(input("Podaj Pierwsza liczbe: "))
+b = float(input("Podaj druga liczbe: "))
+
+print("Suma: ", a+b)
+print("Roznica: ", a-b)
+print("Iloczyn: ", a*b)
+
+if b != 0:
+	print("Iloraz: ", a/b)
+else:
+	print("Nie mozna dzielic przez 0")
